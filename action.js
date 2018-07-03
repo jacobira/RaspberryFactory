566aaca46812381254b99448ae2c4553acafb774
Jacob Baldwin <jacobbaldwin@Jacobs-MacBook-Pro.local>
2018-07-03 08:36:44 -0600

    Initial setup of the site with design

---
+++ action.js
@@ -0,0 +1,39 @@
+toHome();
+
+function toHome() {
+    $(".contentCard").addClass("hidden");
+    $("#homePage").toggleClass("hidden");
+    console.log("Navigated to Home Page");
+}
+
+function toEducation() {
+    $(".contentCard").addClass("hidden");
+    $("#education").toggleClass("hidden");
+    console.log("Navigated to Education Page");
+}
+
+function toSkills() {
+    $(".contentCard").addClass("hidden");
+    $("#skills").toggleClass("hidden");
+    console.log("Navigated to Skills Page");
+}
+
+function toAboutMe() {
+    $(".contentCard").addClass("hidden");
+    $("#aboutMe").toggleClass("hidden");
+    console.log("Navigated to About Me Page");
+}
+
+function toContactInfo() {
+    $(".contentCard").addClass("hidden");
+    $("#contactInfo").toggleClass("hidden");
+    console.log("Navigated to Contact Info Page");
+}
+
+function animateForward(element){
+    $(element).addClass("animateForward");
+}
+
+function removeAnimateForward(element){
+    $(element).removeClass("animateForward");
+}
